d864f75bcdeb45679cb0bf39aa182a137d9c95b7
Gabriel Stewart <bluetoothxeon@hotmail.com>
2012-02-23 13:11:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '962577f0ecf37770983464bfb7a8defd8b14ff6b7ab05a7e05a4fe5d95ed689f760b42db17bd74fe2d4d0860e6f844cd13099361bdb190b2dcb145a3f99be938'
